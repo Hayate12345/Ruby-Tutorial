40d6d0634ea97c7238f833913116bf8a4931a453
hayategari <hayatesato1@gmail.com>
2023-01-20 13:12:47 +0900

    変数のセクション完成

---
+++ variable/variable.rb
@@ -0,0 +1,13 @@
+# ! 静的型付けと動的型付けの違い
+
+# ! 静的型付けとは (JavaやC#などで使われる) （プログラム実行前に型を定義する手法）
+# * コード例（Javaの静的型付け）
+# int num = 100
+# String name = "Takeda"
+
+# ! 動的型付けとは (phpやrubyで使われる) （プログラム実行時に型が自動的に決まる手法）
+num = 100
+name = "Takeda"
+
+puts num.class # ? Integer
+puts name_test.sclass # ? Integer
