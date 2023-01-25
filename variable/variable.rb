49a85ea385a4058f5f76ebc55bfa58a8a76a5199
hayategari <hayatesato1@gmail.com>
2023-01-25 12:30:14 +0900

    リファクタリング

--- variable/variable.rb
+++ variable/variable.rb
@@ -1,8 +1,16 @@
-# ! 問題
-# * 1、自分の年齢を変数ageに代入し、出力してください。
-age = 18
-puts age
+# ! 静的型付けについて (JavaやC#などで使われる) （プログラム実行前に型を定義する手法）
+# * コード例（Javaの静的型付け）
+# int num = 100
+# String name = "Takeda"
 
+# ! 動的型付けについて (phpやrubyで使われる) （プログラム実行時に型が自動的に決まる手法）
+num = 100
+name = "Takeda"
+
+# ? 実行して型を確認してみる
+puts num.class # ? Integer
+puts name.class # ? String
+
+# ! 練習問題
+# * 1、自分の年齢を変数ageに代入し、出力してください。
 # * 2、変数ageを上書きして50を代入し、出力してください。
-age = 50
-puts age
